da8e573cbc52449922dcd830f12718a2bed88a7f
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-05-19 10:28:29 +0300

    function omit is implemented, tests pass successfully

--- 01-javascript-data-types/3-omit/index.js
+++ 01-javascript-data-types/3-omit/index.js
@@ -6,4 +6,31 @@
  */
 export const omit = (obj, ...fields) => {
 
+  let nestedObjOccur = 0;
+  let newObj = {};
+
+  // check does original obj include nested objects
+  for (let value of Object.values(obj)) {
+    if ( typeof(value) === "object" ) {
+      nestedObjOccur +=1;
+    }
+  }
+
+  // perform cloning of original object
+  if (nestedObjOccur === 0) {
+    newObj = Object.assign({}, obj); //simple cloning
+  } else {
+    console.log("Should implement deep cloning including nested objects")
+  }
+
+  let keyOccur = 0;
+
+  for (let key of Object.keys(obj)) {
+    if (fields.includes(key)) {
+      delete newObj[key];
+      keyOccur +=1;
+    }
+  }
+
+  return (keyOccur != 0) ? newObj : obj;
 };
