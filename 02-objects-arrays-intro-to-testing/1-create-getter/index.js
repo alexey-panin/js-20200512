8a0c7dea36ad0d1254c067915bdfe38d4118d90c
AlekseiPanin <aleksei.panin@etteplanmore.com>
2020-05-21 23:11:04 +0300

    create getter is done, tests pass

--- 02-objects-arrays-intro-to-testing/1-create-getter/index.js
+++ 02-objects-arrays-intro-to-testing/1-create-getter/index.js
@@ -3,6 +3,18 @@
  * @param {string} path - the strings path separated by dot
  * @returns {function} - function-getter which allow get value from object by set path
  */
-export function createGetter(path) {
 
+export function createGetter(path) {
+  const arr = path.split(".");
+
+  return (obj) => {
+
+    if (obj && Object.keys(obj).length > 0) {
+
+      return arr.reduce( (accumulator, currentValue) => {
+        
+        return accumulator[currentValue];
+      }, obj);
+    }
+  }
 }
